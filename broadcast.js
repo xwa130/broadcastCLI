f360ff96094de8a6b588ba05979d32bd7aceae94
xwa130 <xwa130@sfu.ca>
2017-03-25 22:08:39 +0000

    add chalk package, everything works: I could send email to other people through command line

--- broadcast.js
+++ broadcast.js
@@ -3,6 +3,9 @@ const program = require('commander');
 const csv = require('csv');
 const fs = require('fs');
 const inquirer = require('inquirer');
+const helper = require('sendgrid').mail;
+const async = require('async');
+const chalk = require('chalk');
 
 program
   .version('1.0.0')
@@ -31,6 +34,26 @@ let parse = csv.parse;
 let stream = fs.createReadStream(program.list)
       .pipe(parse({ delimiter : ',' }));
 
+const __sendEmail = function (to, from, subject, callback) {
+  let template = "Wishing you a Merry Christmas and a prosperous year ahead.";
+  let fromEmail = new helper.Email(from.email, from.name);
+  let toEmail = new helper.Email(to.email, to.name);
+  let body = new helper.Content("text/plain", template);
+  let mail = new helper.Mail(fromEmail, subject, toEmail, body);
+
+  let sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
+  let request = sg.emptyRequest({
+    method: 'POST',
+    path: '/v3/mail/send',
+    body: mail.toJSON(),
+  });
+
+  sg.API(request, function(error, response) {
+    if (error) { return callback(error); }
+    callback();
+  });
+};
+
 stream
   .on('error', function(err) {
     return console.error(err.message);
@@ -40,8 +63,15 @@ stream
     let email = data[2];
     contactList.push({ name : name, email : email });
   })
-  .on('end', function() {
-    inquirer.prompt(questions).then(function(answers) {
-      console.log(answers);
+  .on("end", function () {
+    inquirer.prompt(questions).then(function (ans) {
+      async.each(contactList, function (recipient, fn) {
+        __sendEmail(recipient, ans.sender, ans.subject, fn);
+      }, function (err) {
+        if (err) {
+          return console.error(chalk.red(err.message));
+        }
+        console.log(chalk.green('Success'));
+      });
     });
   });
