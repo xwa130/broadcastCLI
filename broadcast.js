562fa2e2267fcdeabd326f11889cc85c2ef839c4
xwa130 <xwa130@sfu.ca>
2017-03-25 17:27:06 +0000

    able to read csv file

--- broadcast.js
+++ broadcast.js
@@ -1,8 +1,46 @@
 const program = require('commander');
+const csv = require('csv');
+const fs = require('fs');
+const inquirer = require('inquirer');
 
 program
   .version('1.0.0')
   .option('-l, --list [list]', 'list of customers in CSV file')
   .parse(process.argv);
 
-console.log(program.list);
+let questions = [
+  {
+    type : "input",
+    name : "sender.email",
+    message : "Sender's email address - "
+  },
+  {
+    type : "input",
+    name : "sender.name",
+    message : "Sender's name - "
+  },
+  {
+    type : "input",
+    name : "subject",
+    message : "Subject - "
+  }
+];
+let contactList = [];
+let parse = csv.parse;
+let stream = fs.createReadStream(program.list)
+      .pipe(parse({ delimiter : ',' }));
+
+stream
+  .on('error', function(err) {
+    return console.error(err.message);
+  })
+  .on('data', function (data) {
+    let name = data[0] + ' ' + data[1];
+    let email = data[2];
+    contactList.push({ name : name, email : email });
+  })
+  .on('end', function() {
+    inquirer.prompt(questions).then(function(answers) {
+      console.log(answers);
+    });
+  });
